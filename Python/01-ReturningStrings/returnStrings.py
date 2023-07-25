1a9d3b1cdd6bf37fb20b0fdb870d8bd06571a899
Leonardo Ferreira <cadleorogfer@gmail.com>
2023-07-25 08:19:43 -0300

    return true if you are employed and not on vacation

--- Python/01-ReturningStrings/returnStrings.py
+++
@@ -1,6 +0,0 @@
-# Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
-
-
-def greet(name):
-    return f"Hello, {name} how are you doing today?"
-   
---
+++ Python/02-set_alarm/set_alarm.py
@@ -0,0 +1,8 @@
+#return true if you are employed and not on vacation 
+
+def set_alarm(employed, vacation):
+    # Your code here
+    if employed == True and vacation == False:
+        return True
+    else:
+        return False
