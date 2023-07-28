f3db04dd732762f32b67eb0f588d047a319eb16c
Leonardo Ferreira <cadleorogfer@gmail.com>
2023-07-28 18:02:03 -0300

    School Paperwork

---
+++ Java/Paper.java
@@ -0,0 +1,14 @@
+package Java;
+
+public class Paper {
+    public static int paperWork(int n, int m) 
+    {
+      if ((n < 0) || (m < 0)){
+        return 0;
+      }else {
+        return m * n;
+      }
+    }
+    
+    
+}
