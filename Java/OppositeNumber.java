b3fa256b6bb5ab8653e3a0b02fc26aba9f62dc74
Leonardo Ferreira <cadleorogfer@gmail.com>
2023-07-29 17:37:48 -0300

    Opposite Number

---
+++ Java/OppositeNumber.java
@@ -0,0 +1,9 @@
+//Very simple, given an integer or a floating-point number, find its opposite.
+
+public class Kata
+    {
+        public static int opposite(int number)
+        {
+            return number * -1;
+        }
+    }
