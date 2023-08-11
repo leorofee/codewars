b4ff68555134e6646cbe3bb76745781e9d6f6968
Leonardo Ferreira <cadleorogfer@gmail.com>
2023-08-11 15:10:27 -0300

    Convert String to Number

---
+++ Java/StringToNumber.java
@@ -0,0 +1,8 @@
+// We need a function that can transform a string into a number.
+public class StringToNumber {
+  public static int stringToNumber(String str) {
+    return Integer.parseInt(str);
+      
+    }
+  
+}
