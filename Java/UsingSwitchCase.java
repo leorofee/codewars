856eea880cb412128d987052ed3e468c58493146
Leonardo Ferreira <cadleorogfer@gmail.com>
2023-08-16 21:35:40 -0300

    Fixed Adding try in Scanner

--- Java/UsingSwitchCase.java
+++ Java/UsingSwitchCase.java
@@ -2,14 +2,17 @@
 import java.util.Scanner;
 public class UsingSwitchCase {
     public static void main(String args[]) {
-        Scanner in = new Scanner(System.in);
-        int day = in.nextInt();
-        
-        switch(day){
-            case 1, 2, 3, 4, 5 -> System.out.println("Dia da Semana");
-            case 6, 7 -> System.out.println("Fim de semana");
-            default -> System.out.println("Número inválido");
+        try(Scanner in = new Scanner(System.in)){
+            
+            int day = in.nextInt();
+            
+            switch(day){
+                case 1, 2, 3, 4, 5 -> System.out.println("Dia da Semana");
+                case 6, 7 -> System.out.println("Fim de semana");
+                default -> System.out.println("Número inválido");
+            }
         }
+        
         
      
     }
