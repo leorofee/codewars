e2097e206811b76449c9ae2976fb81b4c1145371
Leonardo Ferreira <cadleorogfer@gmail.com>
2023-08-16 23:40:03 -0300

    identation fixed

--- Python/11-sum_array/sum_array.py
+++ Python/11-sum_array/sum_array.py
@@ -21,7 +21,7 @@ We're testing basic loops and math operations. This is for beginners who are jus
 Advanced users may find this extremely easy and can easily write this in one line.
  """
 
- import math
+import math
 def sum_array(a):
     if a == []:
         return 0
