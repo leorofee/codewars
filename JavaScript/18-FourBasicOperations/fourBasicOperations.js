95007254dc532178cc388c3b2449cdef0456dbcb
Leonardo Ferreira <cadleorogfer@gmail.com>
2023-08-18 09:33:40 -0300

    Function that does four basic mathematical operations created

---
+++ JavaScript/18-FourBasicOperations/fourBasicOperations.js
@@ -0,0 +1,19 @@
+// Your task is to create a function that does four basic mathematical operations.
+function fourBasicOperations(operation, value1, value2)
+{
+  switch(operation){
+      case '+':
+        return value1 + value2;
+        break;
+      case '-':
+        return value1 - value2;
+        break;
+      case '*':
+        return value1 * value2;
+        break;
+      case '/':
+        return value1 / value2;
+        break;
+  }
+ 
+}
