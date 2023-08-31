613f5ba6685b9189cd9d346ded9dcc5e86b76705
Leonardo Ferreira <cadleorogfer@gmail.com>
2023-08-30 23:06:59 -0300

    implementation of a function that returns the url with anything after the anchor(#) removed

---
+++ Python/26-remove_anchor_from_url/remove_anchor_from_url.py
@@ -0,0 +1,16 @@
+""" Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+Examples
+
+"www.codewars.com#about" --> "www.codewars.com"
+"www.codewars.com?page=1" -->"www.codewars.com?page=1"
+
+ """
+def remove_url_anchor(url):
+    list(url)
+    ans = ''
+    for i in url:
+        if i != '#':
+            ans += i
+        else:
+            break
+    return ans
